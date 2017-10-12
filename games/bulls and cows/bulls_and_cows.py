667e03230d96c6202658829dc51e49c5a89c09f8
mashashaitz <zelenie0volosy@gmail.com>
2017-10-12 16:13:41 +0300

    Update bulls_and_cows.py

--- games/bulls and cows/bulls_and_cows.py
+++ games/bulls and cows/bulls_and_cows.py
@@ -28,6 +28,87 @@ def find_right_words():
     return words_1
 
 
+def draw_victory():
+    global canvas
+    canvas = tk.Canvas(root, width = 775, height = 256, background="SteelBlue1", bd = 0, relief = "flat")
+    canvas.grid(row = 4, column = 3, columnspan=15)
+
+    canvas.create_rectangle(0, 250, 776, 257, fill = "SpringGreen4", outline = "SpringGreen4")
+    canvas.create_oval(740, -35, 810, 35, fill = "Gold", outline = "Gold")
+
+    for each in [[0, "yellow1", "lightblue", "DeepPink2"], [1, "red2", "DarkOliveGreen2", "hotpink"], [2, "cyan", "green3", "purple"], [3, "tan1", "dim gray", "paleTurquoise1"], [4, "sienna1", "RoyalBlue2", "coral2"], [5, "yellow2", "medium sea green", "maroon1"], [6, "orangered2", "gray56", "aquamarine"]]:
+        canvas.create_oval(60 + each[0]*100, 40, 100 + each[0]*100, 80, fill = "BlanchedAlmond", outline = "BlanchedAlmond")
+        canvas.create_line(80 + each[0]*100, 60, 80 + each[0]*100, 180, fill = "BlanchedAlmond", width = 4)
+        canvas.create_line(80 + each[0]*100, 178, 95 + each[0]*100, 250, fill = "BlanchedAlmond", width = 3)
+        canvas.create_line(79 + each[0]*100, 178, 64 + each[0]*100, 250, fill = "BlanchedAlmond", width = 3)
+        canvas.create_line(81 + each[0]*100, 105, 91 + each[0]*100, 155, fill = "BlanchedAlmond", width = 2)
+        canvas.create_line(79 + each[0]*100, 105, 60 + each[0]*100, 151, fill = "BlanchedAlmond", width = 2)
+        
+        canvas.create_oval(60 + 15 + each[0]*100, 40 + 24, 100 - 15 + each[0]*100, 80 - 7, fill = "Tomato1", outline = "Tomato1")
+        canvas.create_oval(60 + 15 + each[0]*100, 40 + 22, 100 - 15 + each[0]*100, 80 - 9, fill = "BlanchedAlmond", outline = "BlanchedAlmond")
+
+        canvas.create_oval(67 + each[0]*100, 50, 72 + each[0]*100, 57, fill = each[2], outline = each[2])
+        canvas.create_oval(88 + each[0]*100, 50, 93 + each[0]*100, 57, fill = each[2], outline = each[2])
+        canvas.create_oval(68 + each[0]*100, 52, 69 + each[0]*100, 53, fill = "black", outline = "black")
+        canvas.create_oval(89 + each[0]*100, 52, 90 + each[0]*100, 53, fill = "black", outline = "black")
+
+        canvas.create_oval(10 + each[0]*100, 2, 50 + each[0]*100, 75, fill = each[1], outline = each[1])
+        canvas.create_polygon(27 + each[0]*100, 78, 30 + each[0]*100, 75, 33 + each[0]*100, 78, fill = each[1], outline = each[1])
+        canvas.create_line(30 + each[0]*100, 78, 60 + each[0]*100, 151, fill = "orange1", width = 1)
+
+        canvas.create_polygon(87 + each[0]*100, 38, 90 + each[0]*100, 43, 84 + each[0]*100, 41, fill = each[3], outline = each[3])
+        canvas.create_polygon(93 + each[0]*100, 47, 90 + each[0]*100, 43, 96 + each[0]*100, 44, fill = each[3], outline = each[3])
+
+
+def draw_loss():
+    global canvas
+    canvas = tk.Canvas(root, width = 775, height = 256, background="gray54", bd = 0, relief = "flat")
+    canvas.grid(row = 4, column = 3, columnspan=15)
+
+    canvas.create_rectangle(0, 250, 776, 257, fill = "dark olive green", outline = "dark olive green")
+    canvas.create_oval(740, -35, 810, 35, fill = "ghost white", outline = "ghost white")
+
+    for each in [[0, "yellow1", "lightblue", "DeepPink2"], [1, "red2", "DarkOliveGreen2", "hotpink"], [2, "cyan", "green3", "purple"], [3, "tan1", "dim gray", "paleTurquoise1"], [4, "sienna1", "RoyalBlue2", "coral2"], [5, "yellow2", "medium sea green", "maroon1"], [6, "orangered2", "gray56", "aquamarine"]]:
+        canvas.create_oval(60 + each[0]*100, 40, 100 + each[0]*100, 80, fill = "BlanchedAlmond", outline = "BlanchedAlmond")
+        canvas.create_line(80 + each[0]*100, 60, 80 + each[0]*100, 180, fill = "BlanchedAlmond", width = 4)
+        canvas.create_line(80 + each[0]*100, 178, 95 + each[0]*100, 250, fill = "BlanchedAlmond", width = 3)
+        canvas.create_line(79 + each[0]*100, 178, 64 + each[0]*100, 250, fill = "BlanchedAlmond", width = 3)
+        canvas.create_line(81 + each[0]*100, 105, 91 + each[0]*100, 155, fill = "BlanchedAlmond", width = 2)
+        canvas.create_line(79 + each[0]*100, 105, 69 + each[0]*100, 155, fill = "BlanchedAlmond", width = 2)
+        
+        canvas.create_oval(60 + 15 + each[0]*100, 71-2, 100 - 15 + each[0]*100, 80 - 2, fill = "Tomato1", outline = "Tomato1")
+        canvas.create_oval(60 + 15 + each[0]*100, 71, 100 - 15 + each[0]*100, 80, fill = "BlanchedAlmond", outline = "BlanchedAlmond")
+
+        canvas.create_oval(67 + each[0]*100, 50, 72 + each[0]*100, 57, fill = each[2], outline = each[2])
+        canvas.create_oval(88 + each[0]*100, 50, 93 + each[0]*100, 57, fill = each[2], outline = each[2])
+        canvas.create_oval(69 + each[0]*100, 53, 70 + each[0]*100, 54, fill = "black", outline = "black")
+        canvas.create_oval(90 + each[0]*100, 53, 91 + each[0]*100, 54, fill = "black", outline = "black")
+
+
+def draw_regular():
+    global canvas
+    canvas = tk.Canvas(root, width = 775, height = 256, background="SteelBlue1", bd = 0, relief = "flat")
+    canvas.grid(row = 4, column = 3, columnspan=15)
+
+    canvas.create_rectangle(0, 250, 776, 257, fill = "SpringGreen4", outline = "SpringGreen4")
+    canvas.create_oval(740, -35, 810, 35, fill = "Gold", outline = "Gold")
+
+    for each in [[0, "yellow1", "lightblue", "DeepPink2"], [1, "red2", "DarkOliveGreen2", "hotpink"], [2, "cyan", "green3", "purple"], [3, "tan1", "dim gray", "paleTurquoise1"], [4, "sienna1", "RoyalBlue2", "coral2"], [5, "yellow2", "medium sea green", "maroon1"], [6, "orangered2", "gray56", "aquamarine"]]:
+        canvas.create_oval(60 + each[0]*100, 40, 100 + each[0]*100, 80, fill = "BlanchedAlmond", outline = "BlanchedAlmond")
+        canvas.create_line(80 + each[0]*100, 60, 80 + each[0]*100, 180, fill = "BlanchedAlmond", width = 4)
+        canvas.create_line(80 + each[0]*100, 178, 95 + each[0]*100, 250, fill = "BlanchedAlmond", width = 3)
+        canvas.create_line(79 + each[0]*100, 178, 64 + each[0]*100, 250, fill = "BlanchedAlmond", width = 3)
+        canvas.create_line(81 + each[0]*100, 105, 91 + each[0]*100, 155, fill = "BlanchedAlmond", width = 2)
+        canvas.create_line(79 + each[0]*100, 105, 69 + each[0]*100, 155, fill = "BlanchedAlmond", width = 2)
+        
+        canvas.create_line(60 + 16 + each[0]*100, 70, 100 - 16 + each[0]*100, 70, fill = "Tomato1", width = 2)
+
+        canvas.create_oval(67 + each[0]*100, 50, 72 + each[0]*100, 57, fill = each[2], outline = each[2])
+        canvas.create_oval(88 + each[0]*100, 50, 93 + each[0]*100, 57, fill = each[2], outline = each[2])
+        canvas.create_oval(69 + each[0]*100, 53, 70 + each[0]*100, 54, fill = "black", outline = "black")
+        canvas.create_oval(90 + each[0]*100, 53, 91 + each[0]*100, 54, fill = "black", outline = "black")
+
+
 def delete_letter():
     global label_2
     text_of_2 = label_2.config('text')[-1]
@@ -44,7 +125,7 @@ def delete_letter():
         new_text_of_2 = text_of_2[0] + text_of_2[1] + text_of_2[2] + '_'
 
     label_2 = tk.Label(root, text = new_text_of_2, width = 16)
-    label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13, 'italic'))
+    label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
     label_2.grid(row = 3, column = 1)
 
 
@@ -58,8 +139,10 @@ def add_letter(letter):
         new_text_of_2 = text_of_2[0] + letter + '__'
     elif '__' in text_of_2:
         new_text_of_2 = text_of_2[0] + text_of_2[1] + letter + '_'
-    else:
+    elif '_' in text_of_2:
         new_text_of_2 = text_of_2[0] + text_of_2[1] + text_of_2[2] + letter
+    else:
+        new_text_of_2 = text_of_2
 
     label_2 = tk.Label(root, text = new_text_of_2, width = 16)
     label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
@@ -90,19 +173,25 @@ def how_many_bulls():
                 k += 1
         if b == 4:
             label_2 = tk.Label(root, text = 'Победа!!!', width = 16)
-            label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13, 'italic'))
+            label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
             label_2.grid(row = 3, column = 1)
+
+            label_1 = tk.Label(root, text = word, width = 16)
+            label_1.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
+            label_1.grid(row = 2, column = 1)
+
+            draw_victory()
 
             global list_of_words
             if guess + ' 4 0' not in list_of_words:
                 list_of_words.append(guess + ' 4 0')
 
             label_1 = tk.Label(root, text = guess, width = 16)
-            label_1.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13, 'italic'))
+            label_1.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
             label_1.grid(row = 2, column = 1)
 
             listbox = tk.Listbox(root, width = 16, height = 16)
-            listbox.config(bg = "White", bd = 1, fg = "Black", font = ('times', 13, 'italic'))
+            listbox.config(bg = "White", bd = 1, fg = "Black", font = ('times', 13))
             for result in list_of_words:
                 listbox.insert(tk.END, result)
             listbox.grid(row = 4, column = 1)
@@ -124,13 +213,44 @@ def how_many_bulls():
 
         if len(list_of_words) == 16:
             label_2 = tk.Label(root, text = 'Проигрыш(', width = 16)
-            label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13, 'italic'))
+            label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
             label_2.grid(row = 3, column = 1)
             
             label_1 = tk.Label(root, text = word, width = 16)
-            label_1.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13, 'italic'))
+            label_1.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
             label_1.grid(row = 2, column = 1)
+
+            draw_loss()
+
+
+def give_up(event):
+    global label_1
+    global label_2
     
+    label_2 = tk.Label(root, text = 'Вы сдались(', width = 16)
+    label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
+    label_2.grid(row = 3, column = 1)
+            
+    label_1 = tk.Label(root, text = word, width = 16)
+    label_1.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
+    label_1.grid(row = 2, column = 1)
+
+    draw_loss()
+
+
+def give_up_with_style(event):
+    global label_1
+    global label_2
+    
+    label_2 = tk.Label(root, text = 'Вы сдались)', width = 16)
+    label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
+    label_2.grid(row = 3, column = 1)
+
+    label_1 = tk.Label(root, text = word, width = 16)
+    label_1.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
+    label_1.grid(row = 2, column = 1)
+
+    draw_victory()
     
 def find_bulls(button):
     global label_2
@@ -234,6 +354,12 @@ def createall(event):
     label_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
     label_2.grid(row = 3, column = 1)
 
+    global canvas
+    canvas = tk.Canvas(root, width = 775, height = 256, background="White", bd = 0, relief = "flat")
+    canvas.grid(row = 4, column = 3, columnspan=15)
+
+    draw_regular()
+
     create_letters()
     create_letters_2()
     
@@ -250,14 +376,24 @@ def main():
     words = find_right_words()
 
     button = tk.Button(root, text = "Новая игра", width = 16)
-    button.config(bg = "White", bd = 4, fg = "Black", font = ('times', 12, 'italic'))
+    button.config(bg = "White", bd = 4, fg = "Black", font = ('times', 12))
     button.bind('<Button-1>', createall)
     button.grid(row = 1, column = 1)
 
     button_1 = tk.Button(root, text = "+", width = 4)
-    button_1.config(bg = "White", bd = 4, fg = "Black", font = ('times', 12, 'italic'))
+    button_1.config(bg = "White", bd = 4, fg = "Black", font = ('times', 12))
     button_1.bind('<Button-1>', create_letters_3)
     button_1.grid(row = 6, column = 18)
+
+    button_2 = tk.Button(root, text = "Сдаться", width = 16)
+    button_2.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
+    button_2.bind('<Button-1>', give_up)
+    button_2.grid(row = 5, column = 1)
+
+    button_3 = tk.Button(root, text = "Сдаться красиво", width = 16)
+    button_3.config(bg = "White", bd = 4, fg = "Black", font = ('times', 13))
+    button_3.bind('<Button-1>', give_up_with_style)
+    button_3.grid(row = 6, column = 1)
 
     global word
     word = random.choice(words)
@@ -283,7 +419,8 @@ def main():
 
     create_letters()
     create_letters_2()
-    
+
+    draw_regular()
 
     root.mainloop()
     
