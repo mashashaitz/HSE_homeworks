d9f34f15e42b293cec44fd9517839b71548e7f00
mashashaitz <zelenie0volosy@gmail.com>
2017-12-20 22:58:32 +0300

    Update old_orphography.py

--- Borekh/old_orphography.py
+++ Borekh/old_orphography.py
@@ -193,7 +193,7 @@ def construct_new_word(word, stemm, gramm_inf, old_form, gramm_inf_next=''):
             prepos += letter
     span = prepos
     span = span.replace(';', ';</span>')
-    span = span.replace('&', '<span style="text-rendering:auto;">&')
+    span = span.replace('&', '<span>&')
     return span               
 
 
